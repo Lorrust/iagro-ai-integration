ecb9e79506006da489fc1da7c640897bcabd59a4
Guichard² <jeancharles.g.guichard@gmail.com>
2025-04-01 20:06:08 -0300

    add docs to convert

--- scraping/config.py
+++ scraping/config.py
@@ -1,4 +1,10 @@
-BASE_URL = "https://blog.aegro.com.br/pragas-agricolas/"
+# BASE_URL = [
+#         "https://blog.aegro.com.br/pragas-agricolas/",
+#         "https://agro.estadao.com.br/summit-agro/5-principais-pragas-nos-cultivos-de-graos",
+#         "https://folhaagricola.com.br/2024/01/02/pragas-e-doencas-podem-comprometer-ate-90-da-producao-agricola/"
+#     ] DESCOMENTAR ASSIM QUE TODOS OS LINKS DEREM CERTO
+
+BASE_URL = "https://folhaagricola.com.br/2024/01/02/pragas-e-doencas-podem-comprometer-ate-90-da-producao-agricola/"
 HEADERS = {
     "User-Agent": "Mozilla/5.0"
 }
---
+++ scraping/docling/docs/doc_paths.py
@@ -0,0 +1,8 @@
+DOC_PATHS= [
+    "https://www.alice.cnptia.embrapa.br/alice/bitstream/doc/875724/1/CNPTID41892.pdf",
+    "https://www.emater.df.gov.br/wp-content/uploads/2018/06/praticas-insetos-praga.pdf",
+    "https://www.sosbai.com.br/uploads/documentos/criterios-e-normas-para-a-recomendacao-e-avaliacao-de-agrotoxicos-para-a-cultura-de-arroz-irrigado_799.pdf",
+    "https://www.sosbai.com.br/uploads/documentos/recomendacoes-tecnicas-da-pesquisa-para-o-sul-do-brasil_310.pdf",
+    "https://www.infoteca.cnptia.embrapa.br/infoteca/bitstream/doc/371781/1/CPATCDOCUMENTOS70MANEJOECONTROLEDASPRINCIPAISPRAGASDACULTURADOARROZIRRIGADONAREGIA.pdf",
+    "https://ciram.epagri.sc.gov.br/ciram_arquivos/agroconnect/boletins/BT_PragasDoencasMilho.pdf",
+]
